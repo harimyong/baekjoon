81c30cb78ab80efeaac56bf02911d9da02fa82c2
harimyong <89829674+harimyong@users.noreply.github.com>
2023-08-10 01:19:05 +0900

    [Bronze III] Title: N M 찍기, Time: 272 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/18883. N M 찍기/N M 찍기.py
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.py
@@ -1,13 +1,6 @@
-def print_N_M(N, M):
-    for n in range(1, N+1):
-        row_list = []
-        for m in range(1, M+1):
-            row_list.append(str(m + (n-1) * M))
-            
-        print(" ".join(row_list))
-    
-    
-if __name__ == "__main__":
-    N, M = map(int, input().split())
-    
-    print_N_M(N=N, M=M)
+n,m=map(int,input().split())
+i=1
+for _ in range(n):
+    arr=[j for j in range(i,i+m)]
+    i+=m
+    print(' '.join(list(map(str,arr))))
