d873d05fa96c082ba1f158b85b90844fb4674465
harimyong <89829674+harimyong@users.noreply.github.com>
2023-10-21 17:59:54 +0900

    [Gold IV] Title: 이분 그래프, Time: 1272 ms, Memory: 53208 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,36 +1,34 @@
+t=int(input())
 import sys
-sys.setrecursionlimit(1000000)
-input = sys.stdin.readline
-
-K = int(input())
-
-
-def dfs(start, group):
-    visited[start] = group  # 해당 정점의 group 설정(1,-1)
-
-    for i in graph[start]:
-        if not visited[i]:  # 만약 방문하지 않았다면
-            a = dfs(i, -group)  # 그룹값을 -1로 주어 dfs를 돈다.
-            if not a:  # 만약 a가 false가 나왔다면
-                return False  # False를 리턴
-        elif visited[i] == visited[start]:  # 만약 현재 정점과 연결된 정점의 그룹값이 같다면
-            return False  # False를 리턴
-    return True  # 그외의 경우는 True를 리턴
-
-
-for _ in range(K):
-    V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]  # 빈 그래프 생성
-    visited = [False] * (V + 1)  # 방문한 정점 체크
-    for _ in range(E):
-        a, b = map(int, input().split())
+input=sys.stdin.readline
+sys.setrecursionlimit(10**5)
+for _ in range(t):
+    v,e=map(int,input().split())
+    visit=[0 for _ in range(v+1)]
+    graph=[[] for _ in range(v+1)]
+    for _ in range(e):
+        a,b=map(int,input().split())
         graph[a].append(b)
         graph[b].append(a)
-
-    for i in range(1, V + 1):
-        if not visited[i]:  # 방문한 정점이 아니면, dfs 수행
-            result = dfs(i, 1)
-            if not result:  # 만약 result가 False가 나왔다면 더이상 수행할 필요가 없으므로
-                break  # break
-
-    print("YES" if result else "NO")
+    
+    def dfs(k,now):
+        visit[now]=k
+        for nx in graph[now]:
+            if visit[nx]==0:                
+                res= dfs(-k,nx)
+                if not res:
+                    return False
+            else:
+                if visit[nx]==k:
+                    return False
+        return True 
+    
+    for i in range(1,v+1):
+        if visit[i]==0:
+            res=dfs(1,i)
+            if not res:
+                break
+    if res:
+        print('YES')
+    else:
+        print('NO')
