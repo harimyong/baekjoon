4267d0a2ce5c6f5625dc14239f6c2208f43cf449
harimyong <89829674+harimyong@users.noreply.github.com>
2023-11-16 16:09:32 +0900

    [Silver I] Title: 스타트와 링크, Time: 1148 ms, Memory: 242240 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,30 +1,32 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-board = [list(map(int,input().split())) for _ in range(N)]
-visited = [False for _ in range(N)]
-INF = 2147000000
-res = INF
 
-# ✨ DFS
-def DFS(L,idx):
-    global res
-    if L == N//2:
-        A = 0
-        B = 0
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    A += board[i][j]
-                elif not visited[i] and not visited[j]:
-                    B +=board[i][j]
-        res = min(res, abs(A-B))
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10**5)
+n=int(input())
+arr=[list(map(int,input().split())) for _ in range(n)]
+
+visit=[0 for _ in range(n)]
+ans=2147000000
+
+def back(idx,cnt):
+    if cnt==n//2:
+        global ans
+        tota=0
+        totb=0
+        for i in range(n):
+            for j in range(n):
+                if visit[i] and visit[j]:
+                    tota+=arr[i][j]
+                elif not visit[i] and not visit[j]:
+                    totb+=arr[i][j]
+        ans=min(ans,abs(tota-totb))
         return
-    for i in range(idx,N):
-        if not visited[i]:
-            visited[i] = True
-            DFS(L+1,i+1)
-            visited[i] = False
-            
-DFS(0,0)
-print(res)
+
+    for i in range(idx,n):
+        if not visit[i]:
+            visit[i]=1
+            back(i+1,cnt+1)
+            visit[i]=0
+
+back(0,0)
+print(ans)
