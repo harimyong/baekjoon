6237f009aa90a5d9986f0fdebd26e47e83057fe0
harimyong <89829674+harimyong@users.noreply.github.com>
2024-01-09 19:34:39 +0900

    [Diamond V] Title: 수열과 쿼리 18, Time: 844 ms, Memory: 22228 KB -BaekjoonHub

---
+++ 백준/Diamond/14504. 수열과 쿼리 18/수열과 쿼리 18.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <map>
+#define MAX 200001
+#define INF 100000000000
+using namespace std;
+typedef long long ll;
+int n,q,sqrtN,bucket_size;
+int arr[MAX];
+vector<int> bucket[MAX*4];
+
+void init(){
+	cin >> n;
+	sqrtN=sqrt(n);
+	//cout << sqrtN << '\n';
+	for(int i=1;i<=n;i++){
+		cin >> arr[i];
+		if(i%sqrtN==0) bucket_size++;
+		bucket[i/sqrtN].push_back(arr[i]);
+	}//cout << '\n';
+	for(int i=0;i<=bucket_size;i++) {
+		sort(bucket[i].begin(),bucket[i].end());
+	}
+}
+
+void update(int k,int v){
+	int Ai=arr[k];
+	for(int i=0;i<bucket[k/sqrtN].size();i++){
+		if(arr[k]==bucket[k/sqrtN][i]){
+			bucket[k/sqrtN][i]=v;
+			arr[k]=v;
+			break;
+		}
+	}
+	sort(bucket[k/sqrtN].begin(),bucket[k/sqrtN].end());
+}
+
+int query(int l,int r,int k){
+	int ret=0;
+	if(l/sqrtN==r/sqrtN){
+		for(int i=l;i<=r;i++) if(arr[i]>k) ret++;
+	}else{
+		for(int i=l;i<sqrtN*(l/sqrtN+1);i++) if(arr[i]>k) ret++;
+		for(int i=sqrtN*(r/sqrtN);i<=r;i++) if(arr[i]>k) ret++;
+		for(int i=l/sqrtN+1;i<r/sqrtN;i++) 
+		ret+=bucket[i].end()-upper_bound(bucket[i].begin(),bucket[i].end(),k);
+	}
+	return ret;
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	init();
+	cin >> q;
+	while(q--){
+		int c;
+		cin >> c;
+		if(c==1){
+			int a,b,k; cin >> a >> b >> k;
+			cout << query(a,b,k) << '\n';
+		}else{
+			int k,v; cin >> k >> v;
+			update(k,v);
+		}
+	}
+	
+	
+ 	return 0;
+ }
