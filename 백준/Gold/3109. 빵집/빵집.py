b76ee1bdddbd54faa3b543b0426101353c6f0815
harimyong <89829674+harimyong@users.noreply.github.com>
2024-01-29 23:45:24 +0900

    [Gold II] Title: 빵집, Time: 1800 ms, Memory: 373192 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -3,8 +3,8 @@ from collections import deque
 n,m=map(int,input().split())
 arr=[list(input().rstrip()) for _ in range(n)]
 
-
 def dfs(x,y):
+    arr[x][y]='x'
     dx=[-1,0,1]
     dy=[1,1,1]
     
@@ -15,15 +15,12 @@ def dfs(x,y):
         xx=dx[i]+x
         yy=dy[i]+y
         if 0<=xx<n and 0<=yy<m:
-            if arr[xx][yy]!='x' and visit[xx][yy]==0:
-                visit[xx][yy]=1
-                if dfs(xx,yy)==1:
+            if arr[xx][yy]!='x':
+                if dfs(xx,yy):
                     return 1
     return 0
 
 ans=0
-visit=[[0 for _ in range(m)] for _ in range(n)]
 for i in range(n):
-    if dfs(i,0)==1: 
-        ans+=1
+    ans+=dfs(i,0)
 print(ans)
