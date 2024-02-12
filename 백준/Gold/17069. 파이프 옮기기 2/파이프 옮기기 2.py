76e1da3df2a291d20085ac29622e360461502476
harimyong <89829674+harimyong@users.noreply.github.com>
2024-02-12 12:33:13 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.py
@@ -0,0 +1,14 @@
+import sys; input=sys.stdin.readline
+dp=[[[0 for _ in range(3)] for _ in range(35)] for _ in range(35)]
+dp[0][1][0]=1
+n=int(input())
+arr=[list(map(int,input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i+1<n and j+1<n and arr[i+1][j+1]==0 and arr[i][j+1]==0 and arr[i+1][j]==0:
+            dp[i+1][j+1][2]+=dp[i][j][0]+dp[i][j][1]+dp[i][j][2]
+        if j+1<n and arr[i][j+1]==0:
+            dp[i][j+1][0]+=dp[i][j][0]+dp[i][j][2]
+        if i+1<n and arr[i+1][j]==0:
+            dp[i+1][j][1]+=dp[i][j][1]+dp[i][j][2]
+print(sum(dp[n-1][n-1]))
