762b64eddb790cda1e80d485337ca65838ff1a8d
harimyong <89829674+harimyong@users.noreply.github.com>
2024-02-15 17:54:34 +0900

    [Gold V] Title: 강의실, Time: 420 ms, Memory: 121520 KB -BaekjoonHub

---
+++ 백준/Gold V/1374. 강의실/강의실.py
@@ -0,0 +1,16 @@
+import sys; input=sys.stdin.readline
+n=int(input())
+arr=[]
+for _ in range(n):
+    a,b,c=map(int,input().split())
+    arr.append((b,c))
+import heapq
+pq=[]
+arr.sort()
+for a,b in arr:
+    if pq and a>=pq[0]:
+        heapq.heappop(pq)
+    heapq.heappush(pq,b)
+print(len(pq))
+    
+
