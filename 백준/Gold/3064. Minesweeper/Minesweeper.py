0ec85499cb78f7d6611060a26578dcbf0f7f04ea
harimyong <89829674+harimyong@users.noreply.github.com>
2024-02-19 02:31:39 +0900

    [Gold IV] Title: Minesweeper, Time: 164 ms, Memory: 111984 KB -BaekjoonHub

---
+++ 백준/Gold/3064. Minesweeper/Minesweeper.py
@@ -0,0 +1,39 @@
+import sys; input=sys.stdin.readline
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=[list(input().rstrip()) for _ in range(n)]
+    if n<=2:
+        print(0)
+        continue
+    if n==3:
+        if arr[0][0]=='1':
+            print(1)
+            continue
+        else:
+            print(0)
+            continue
+    ans=0
+    dx=[-1,-1,-1,0,0,1,1,1]
+    dy=[-1,0,1,-1,1,-1,0,1]
+    for i in range(n):
+        for j in range(n):
+            flag=True
+            if arr[i][j]=='#':
+                for k in range(8):
+                    xx=dx[k]+i
+                    yy=dy[k]+j
+                    if arr[xx][yy]=='0':
+                        flag=False
+                        break
+                if flag:
+                    for k in range(8):
+                        xx=dx[k]+i
+                        yy=dy[k]+j
+                        if arr[xx][yy]!='#' and arr[xx][yy]!='*':
+                            arr[xx][yy]=str(int(arr[xx][yy])-1)
+                    arr[i][j]='*'
+                    ans+=1
+    print(ans)
+            
+                
