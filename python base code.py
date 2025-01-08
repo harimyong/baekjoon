40b111f5604c10ccaa0545e4623c8f074907d209
harimyong <89829674+harimyong@users.noreply.github.com>
2025-01-08 20:04:45 +0900

    Update python base code.py

--- python base code.py
+++ python base code.py
@@ -2,10 +2,12 @@ import sys; input=sys.stdin.readline
 from heapq import heappush as psh
 from heapq import heappop as pp
 from collections import deque
-In,St,Ma,Di=int,str,map,dict
+In,St,Ma,Di,q=int,str,map,dict,deque()
 def M_IS(type): return map(type,input().split()) 
 def Inp_OnD(type): return list(M_IS(type)) #Set 1d list of a specific type
 def Inp_TwD(type,N): return [list(M_IS(type)) for _ in range(N)] #Set 2d list of a specific type
 def Inp_ThD(type,N,M): return [[list(M_IS(type)) for _ in range(M)] for _ in range(N)] #Set 3d list of a specific type
 def Set_OnD(base,N): return [base for _ in range(N)] #Set 1d list with base
 def Set_TwD(base,N,M): return [[base for _ in range(M)] for _ in range(N)] #Set 2d list with base
+
+
