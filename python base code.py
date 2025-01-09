dc36258aaceee818a568d28249dc7aa5cc4d6499
harimyong <89829674+harimyong@users.noreply.github.com>
2025-01-09 19:21:22 +0900

    Update python base code.py

--- python base code.py
+++ python base code.py
@@ -12,3 +12,4 @@ def Inp_ThD(type,N,M): return [[list(M_IS(type)) for _ in range(M)] for _ in ran
 def Set_OnD(base,N): return [base for _ in range(N)] #Set 1d list with base
 def Set_TwD(base,N,M): return [[base for _ in range(M)] for _ in range(N)] #Set 2d list with base
 def Set_ThD(base,N,M,K): return [[[base for _ in range(K)] for _ in range(M)] for _ in range(N)] #Set 3d list with base
+########################################################################################################################
