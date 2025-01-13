9e30daceb2405e8f0ab13432f580835770176e79
harimyong <89829674+harimyong@users.noreply.github.com>
2025-01-13 20:21:31 +0900

    Update cppbasecode.cpp

--- cppbasecode.cpp
+++ cppbasecode.cpp
@@ -1,4 +1,11 @@
 #include <bits/stdc++.h>
+#define FOR(i,A,B) for(int i=A;i<B;i++)
+#define pii pair<int,int>
+#define INF 987654321
+#define qu queue
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
+using namespace std;
 
 using namespace std;
 
